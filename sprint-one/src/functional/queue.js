8a226fadcfccae80503516e7fbe1429d2a5a3805
Hack Reactor Student <student@hackreactor.com>
2017-08-23 14:47:02 -0700

    Pass all tests for queue for functional pattern

--- sprint-one/src/functional/queue.js
+++ sprint-one/src/functional/queue.js
@@ -3,16 +3,27 @@ var Queue = function() {
 
   // Use an object with numeric keys to store values
   var storage = {};
+  var head = 0;
+  var count = 0;
 
   // Implement the methods below
 
   someInstance.enqueue = function(value) {
+    storage[head + count] = value;
+    count++;
   };
 
   someInstance.dequeue = function() {
+    if (count > 0) {
+      var value = storage[head];
+      head++;
+      count--;
+      return value;
+    }
   };
 
   someInstance.size = function() {
+    return count;
   };
 
   return someInstance;
