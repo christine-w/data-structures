412884a5650ef89179b456d134d65b09445814cd
Hack Reactor Student <student@hackreactor.com>
2017-08-23 20:24:15 -0700

    Pass all initial tests for hashTable

--- sprint-two/src/hashTable.js
+++ sprint-two/src/hashTable.js
@@ -7,14 +7,30 @@ var HashTable = function() {
 
 HashTable.prototype.insert = function(k, v) {
   var index = getIndexBelowMaxForKey(k, this._limit);
+  while (this._storage.get(index) !== undefined) {
+    if (this._storage.get(index)[0] === k) {
+      break;
+    }
+    index++;
+    index %= this._limit;
+  }
+  this._storage.set(index, [k, v]);
 };
 
 HashTable.prototype.retrieve = function(k) {
   var index = getIndexBelowMaxForKey(k, this._limit);
+  if (this._storage.get(index)) {
+    while (this._storage.get(index)[0] !== k) {
+      index++;
+      index %= this._limit;
+    }
+    return this._storage.get(index)[1];
+  }
 };
 
 HashTable.prototype.remove = function(k) {
   var index = getIndexBelowMaxForKey(k, this._limit);
+  this._storage.set(index, undefined);
 };
 
 
