e985234c1cdd0b31d9523e7810b187f80e85f921
Hack Reactor Student <student@hackreactor.com>
2017-08-24 20:08:32 -0700

    Add test to check that set can accept values of any type

--- sprint-two/spec/setSpec.js
+++ sprint-two/spec/setSpec.js
@@ -11,7 +11,7 @@ describe('set', function() {
     expect(set.remove).to.be.a('function');
   });
 
-  it('should add values to a set', function() {
+  it('should add string values to a set', function() {
     set.add('Susan Sarandon');
     set.add('Danny Glover');
     expect(set.contains('Danny Glover')).to.equal(true);
@@ -29,6 +29,24 @@ describe('set', function() {
     set.add('Mel Gibson');
     set.remove('Mel Gibson');
     expect(set.contains('Mel Gibson')).to.equal(false);
+  });
+
+  it('should add any primitive values to a set', function() {
+    set.add(1);
+    set.add(true);
+    set.add(null);
+    expect(set.contains(1)).to.equal(true);
+    expect(set.contains(true)).to.equal(true);
+    expect(set.contains(null)).to.equal(true);
+  });
+
+  it('should add arrays, objects and functions to a set', function() {
+    set.add([1, 2, 3]);
+    set.add({ 1: 'a', 2: 'b', 3: 'c'});
+    set.add(function() {});
+    expect(set.contains([1, 2, 3])).to.equal(true);
+    expect(set.contains({ 1: 'a', 2: 'b', 3: 'c'})).to.equal(true);
+    expect(set.contains(function() {})).to.equal(true);
   });
 
 });
