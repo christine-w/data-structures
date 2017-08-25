3822b1e7fcd19d862cc3e7defab50997b129fbcf
Hack Reactor Student <student@hackreactor.com>
2017-08-25 16:30:01 -0700

    Implement depthFirstLog and getBalanceFactors for AVLTree
    - Also added a searchNode function for testing purposes
    - Also implemented test for the above-mentioned functions

--- sprint-two/spec/avlTreeSpec.js
+++ sprint-two/spec/avlTreeSpec.js
@@ -1,24 +1,71 @@
 describe('avlTree', function() {
 
   var avlTree;
+  var testValues = [100, 80, 102, 52, 93, 180, 85, 97, 98, 99];
+  var testValueDepths = [0, 1, 1, 2, 2, 2, 3, 3, 4, 5];
+  var testValueBF = [-3, 3, 1, 0, 2, 0, 0, 2, 1, 0];
+  var testValueParents = [null, 100, 100, 80, 80, 102, 93, 93, 97, 98];
 
   beforeEach(function() {
-    avlTree = new AVLTree(5);
+    avlTree = new AVLTree(100, 100);
   });
 
-  it('should insert values into the tree', function() {
-    avlTree.insert(8);
-    expect(avlTree.contains(8)).to.equal(true);
-    avlTree.insert(3);
-    avlTree.insert(12);
-    avlTree.insert(1);
-    avlTree.insert(7);
-    avlTree.insert(-2);
-    expect(avlTree.contains(3)).to.equal(true);
-    expect(avlTree.contains(12)).to.equal(true);
-    expect(avlTree.contains(1)).to.equal(true);
-    expect(avlTree.contains(7)).to.equal(true);
-    expect(avlTree.contains(-2)).to.equal(true);
+  it('should insert values into the AVL Tree', function() {
+    for (var i = 0; i < 10000; i++) {
+      var randomVal = Math.random();
+      avlTree.insert(randomVal, randomVal);
+      expect(avlTree.search(randomVal)).to.equal(randomVal);
+    }
+  });
+
+  it('should correctly determine the parent of a node', function() {
+    for (var i = 1; i < testValues.length; i++) {
+      avlTree.insert(testValues[i], testValues[i]);
+    }
+    for (var i = 1; i < testValues.length; i++) {
+      var node = avlTree._searchNode(testValues[i]);
+      expect(node.parent.value).to.equal(testValueParents[i]);
+    }
+  });
+
+  it('should apply a callback on each node of the tree using depth-first traversal', function() {
+    var result = [];
+    var expected = [100, 80, 52, 93, 85, 97, 98, 99, 102, 180];
+    var cb = function(node) {
+      result.push(node.value);
+    };
+    for (var i = 1; i < testValues.length; i++) {
+      avlTree.insert(testValues[i], testValues[i]);
+    }
+    avlTree.depthFirstLog(cb);   
+    expect(result).to.deep.equal(expected);
+
+  });
+
+  it('should correctly determine node depth for an AVL Tree node', function() {
+    // We are only using testValues to make sure balance factors are correctly 
+    // calculated before our implementation of balancing
+    for (var i = 1; i < testValues.length; i++) {
+      avlTree.insert(testValues[i], testValues[i]);
+    }
+    for (var i = 0; i < testValues.length; i++) {
+      var node = avlTree._searchNode(testValues[i]);
+      expect(node.depth).to.equal(testValueDepths[i]);
+    }
+  });
+
+  it('should correctly determine node balance factor for an AVL Tree node (before implementation of balancing)', function() {
+    // We are only using testValues to make sure balance factors are correctly 
+    for (var i = 1; i < testValues.length; i++) {
+      avlTree.insert(testValues[i], testValues[i]);
+    }
+    for (var i = 0; i < testValues.length; i++) {
+      var node = avlTree._searchNode(testValues[i]);
+      expect(node.getBalanceFactor()).to.equal(testValueBF[i]);
+    }
+  });  
+
+  it('should correctly determine node blanace factor for an AVL Tree node as node are inserted', function() {
   });
 
 });
--- sprint-two/src/avlTree.js
+++ sprint-two/src/avlTree.js
@@ -1,53 +1,99 @@
-var AVLTree = function(value) {
+var AVLTree = function(key, value) {
+  this.parent = null;
   this.left = null;
   this.right = null;
+  this.key = key;
   this.value = value;
+  this.depth = 0;
+  this.balanceFactor = 0;
 };
 
-AVLTree.prototype.insert = function(value) {
-  if (this.value > value) {
-    if (this.left) {
-      this.left.insert(value);
+AVLTree.prototype.insert = function(key, value) {
+  var newNode = new AVLTree(key, value);
+  var currentNode = this;
+  while (currentNode) {
+    newNode.depth++;
+    if (currentNode.key > key) {
+      if (currentNode.left) {
+        currentNode = currentNode.left;    
+      } else {
+        currentNode.left = newNode;
+        newNode.parent = currentNode;
+        break;
+      }
     } else {
-      this.left = new AVLTree(value);
-    }
-  } else {
-    if (this.right) {
-      this.right.insert(value);
-    } else {
-      this.right = new AVLTree(value);
+      if (currentNode.right) {
+        currentNode = currentNode.right;
+      } else {
+        currentNode.right = newNode;
+        newNode.parent = currentNode;
+        break;
+      }
     }
   }
 };
 
-AVLTree.prototype._search = function(value) {
-//returns AVLTree -- for testing purposes
-};
-
-AVLTree.prototype.contains = function(value) {
-  if (this.value === value) {
-    return true;
-  } else if (this.value > value) {
+AVLTree.prototype.search = function(key) {
+  if (this.key === key) {
+    return this.value;
+  } else if (this.key > key) {
     if (this.left) {
-      return this.left.contains(value);
-    } else {
-      return false;
-    }
+      return this.left.search(key);
+    } 
   } else {
     if (this.right) {
-      return this.right.contains(value);
-    } else {
-      return false;
+      return this.right.search(key);
     }
-  }
-  
+  } 
+};
+
+AVLTree.prototype._searchNode = function(key) {
+  var resultNode;
+  var cb = function(node) {
+    if (node.key === key) {
+      resultNode = node;
+    }
+  };
+  this.depthFirstLog(cb);
+  return resultNode;
 };
 
 AVLTree.prototype.depthFirstLog = function(callback) {
+  var node = this;
+  callback(node);
+  if (node.left) {
+    node.left.depthFirstLog(callback);
+  }
+  if (node.right) {
+    node.right.depthFirstLog(callback);
+  }
 };
 
-AVLTree.prototype.getHeight = function() {
+// AVLTree.prototype.updateHeight = function() {
+// };
+
+AVLTree.prototype.updateBalanceFactor = function() {
 };
 
 AVLTree.prototype.getBalanceFactor = function() {
+  var maxRightDepth = this.depth;
+  var maxLeftDepth = this.depth;
+  var cbRight = function(node) {
+    if (node.depth > maxRightDepth) {
+      maxRightDepth = node.depth;
+    }
+  };
+  var cbLeft = function(node) {
+    if (node.depth > maxLeftDepth) {
+      maxLeftDepth = node.depth;
+    }
+  };
+  if (this.right) {
+    this.right.depthFirstLog(cbRight); 
+  }
+  if (this.left) {
+    this.left.depthFirstLog(cbLeft);
+  }
+
+  return maxRightDepth - maxLeftDepth;
 };
